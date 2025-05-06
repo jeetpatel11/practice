b1bec4a2be0db116c13771c97c1ced48969371ee
jeetpatel11 <pateljeetr5012@gmail.com>
2025-05-06 15:25:21 +0530

    it for css

---
+++ index.js
@@ -0,0 +1,13 @@
+function myfunction() {
+  console.log("Hello from myfunc");
+}
+
+const counter = 0;
+const increment = () => {
+  counter++;
+  console.log(counter);
+}
+const decrement = () => {
+  counter--;
+  console.log(counter);
+};
